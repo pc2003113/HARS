7fdb036ea21c900a7f956181425b8e964514b27f
PriyaC <priyacjose@gmail.com>
2019-07-20 12:29:42 -0500

    your awesomely descriptive, yet pithy commit message goes here

---
+++ src/main/java/com/hzn/HARS/HarsApplication.java
@@ -0,0 +1,13 @@
+package com.hzn.HARS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hzn/HARS/HarsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hzn.HARS;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HarsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
